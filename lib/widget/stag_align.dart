a09a3f7298941fc5f6753933a49b6f8fe2b92357
Danish Ahmed Siddiqui <siddiquidanish973@gmail.com>
2024-01-16 21:53:41 +0530

    Initial Commit

---
+++ lib/widget/stag_align.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class stg_align extends StatelessWidget {
+  const stg_align({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlignedGridView.count(
+      scrollDirection: Axis.vertical,
+      crossAxisCount: 8,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.network('https://source.unsplash.com/random?sig=$index'),
+        );
+      },
+    );
+  }
+}
